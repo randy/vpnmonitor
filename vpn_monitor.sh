fd36d0457ac91dfe6cc0729a3318931907790ead
Randy <rd.schreck@gmail.com>
2025-02-12 14:29:09 -0700

    update script to notify with a prompt to snooze

--- vpn_monitor.sh
+++ vpn_monitor.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 # this script watches for a VPN connection that has been active for over an hour
+# once a VPN has been connected for an hour, the user is prompts to snooze for
+# an hour; if they respond "no" they are prompted again in 5 minutes, otherwise an hour.
 
+SNOOZE_FILE="/tmp/vpnmonitor_snooze"
 VPN_START=0
-THRESHOLD=3600  # 1 hour in seconds
+THRESHOLD=3600  # 1 hour in seconds (3600)
+SLEEP=300       # sleep for 5 minutes
+
+prompt() {
+  osascript -e 'display dialog "VPN has been on for an hour. Do you want to snooze this notification for an hour?" buttons {"Yes", "No"} default button "Yes"'
+}
+
 while true; do
     if scutil --nc list | grep -q "Connected"; then
         if [ $VPN_START -eq 0 ]; then
@@ -12,11 +21,23 @@ while true; do
             ELAPSED=$((CURRENT_TIME - VPN_START))
             
             if [ $ELAPSED -ge $THRESHOLD ]; then
-                osascript -e 'display notification "VPN has been connected for over an hour" with title "VPN Alert"'
+                if [ -f "$SNOOZE_FILE" ]; then
+                    LAST_SNOOZE=$(cat "$SNOOZE_FILE")
+                    if [ $((CURRENT_TIME - LAST_SNOOZE)) -lt $THRESHOLD ]; then
+                        sleep $THRESHOLD - $SLEEP    # line 42 will sleep for 5 minutes 
+                        continue
+                    else
+                        rm "$SNOOZE_FILE"
+                    fi
+                fi
+                USER_RESPONSE=$(prompt)
+                if [[ "$USER_RESPONSE" == *"Yes"* ]]; then
+                    date +%s > "$SNOOZE_FILE"
+                fi
             fi
         fi
     else
         VPN_START=0
     fi
-    sleep 300  # Wait 5 minutes
+    sleep $SLEEP
 done
